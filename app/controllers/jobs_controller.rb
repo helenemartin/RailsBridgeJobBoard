0a8049590f58f654cbe9c786e7268db80d208c53
Helene <helenemartin13@hotmail.com>
2017-07-06 14:32:43 +0000

    add a single view for each job posting

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -3,12 +3,32 @@ class JobsController < ApplicationController
         @jobs = Job.all
     end
     
+    def show
+        @job = Job.find_by_id(params[:id])
+    end
+    
     def new
         @job = Job.new
     end
     
     def create
         Job.create(job_params)
+        redirect_to jobs_path
+    end
+    
+    def edit
+        @job = Job.find(params[:id])
+    end
+    
+    def update
+        @job = Job.find(params[:id])
+        @job.update_attributes(job_params)
+        redirect_to jobs_path
+    end
+    
+    def destroy
+        @job = Job.find(params[:id])
+        @job.destroy
         redirect_to jobs_path
     end
 private
